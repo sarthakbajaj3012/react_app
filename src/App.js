b8c780c1606045e6da3610f96611a2ff6622bd8a
sarthak-bajaj30 <s.bajaj@student.vu.nl>
2022-07-11 11:27:32 +0200

    reactapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h1>hi this is me trying</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
